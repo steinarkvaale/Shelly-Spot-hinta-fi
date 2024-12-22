f298a914fceb9a4e875bfd7e1f8b75a5538710af
Steinar Kvaale <112165436+steinarkvaale@users.noreply.github.com>
2024-12-22 07:40:03 +0100

    Update Shelly-WaterHeater-Transfer.js
    Updated with region and local settings

--- Scripts/Shelly-WaterHeater-Transfer.js
+++ Scripts/Shelly-WaterHeater-Transfer.js
@@ -2,15 +2,16 @@
 // Supported Shelly firmware versions: 1.0.3 - 1.4.4. Script version: 2024-11-02
 
 // SETTINGS
+let Region = "NO1"; // Supported regions: DK1, DK2, EE, FI, LT, LV, NO1, NO2, NO3, NO4, NO5, SE1, SE2, SE3, SE4
 let Ranks = [1, 2, 3]; // List the 'ranks' (i.e., the hour's order number by price) when the relays are activated
 let Relays = [0]; // Numbers of the relays to be controlled. For example, [0,1,2] to control three relays
 let NightHours = [22, 23, 0, 1, 2, 3, 4, 5, 6]; // Night shift hours. Normally, no need to change these (even during daylight saving time changes).
-let PriceDifference = -1.43; // How much cheaper is the electricity transfer price during night shift?
+let PriceDifference = -1.02; // How much cheaper is the electricity transfer price during night shift?
 let AllowedPrice = 0; // Daytime price that is always allowed. Night hours allow prices higher by the PriceDifference amount.
 let BackupHours = [3, 4, 5]; // Hours when the relay is activated if control data cannot be retrieved.
 
 // CODE
-let url = "https://api.spot-hinta.fi/PlanAhead?priorityHours=" + NightHours.join() + "&priceModifier=" + PriceDifference + "&ranksAllowed=" + Ranks.join() + "&priceAlwaysAllowed=" + AllowedPrice;
+let url = "https://api.spot-hinta.fi/PlanAhead?priorityHours=" + NightHours.join() + "&priceModifier=" + PriceDifference + "&ranksAllowed=" + Ranks.join() + "&priceAlwaysAllowed=" + AllowedPrice + "?region=" + Region;
 let hour = -1;
 let nextMessage = new Date(new Date().getTime() + 2 * 60 * 1000);
 let previousAction = "";
